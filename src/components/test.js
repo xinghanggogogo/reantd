c497237d0c460cd8d6c3b5353dd2cbc422cabe6b
邢航 <xinghang@fullStack.local>
2017-01-08 10:56:46 +0800

    update

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+class  extends React.Component {
+  render () {
+      <div>
+        <div className="test"></div>
+      </div>
+  }
+}
+
+export default ;
